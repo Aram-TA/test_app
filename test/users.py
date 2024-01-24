336b75e071dc8962732c6c06a5e47959f2be67d9
Aram-TA <aramg267@gmail.com>
2024-01-24 10:27:04 +0400

    New validation system in users, dynamic function calls

--- test/users.py
+++ test/users.py
@@ -7,33 +7,15 @@ import config
 
 
 class Users:
+    __slots__ = "users_path",
     """
-    Validation class for out inputs from html forms, it takes then validates
+    Class for our inputs from html forms, it takes then validates
     inputs from routing functions
     """
-    __slots__ = (
-        'request_form',
-        'user_data',
-        'email',
-        'phone_number',
-        'username',
-        'password_repeat',
-        'password',
-        'login_mode'
-    )
-
     def __init__(self) -> None:
-        self.request_form: dict = None
-        self.user_data: dict = None
-        self.email: str = None
-        self.phone_number: str = None
-        self.username: str = None
-        self.password_repeat: str = None
-        self.password: str = None
-        self.login_mode: bool = False
+        self.users_path = config.users_path
 
-    @staticmethod
-    def register_new_account(request_form: dict) -> None:
+    def register_new_account(self, request_form: dict) -> None:
         """
         Loads data from database and after inserts new values there
 
@@ -50,7 +32,7 @@ class Users:
         None
 
         """
-        with open(config.users_path, "r+") as users_json:
+        with open(self.users_path, "r+") as users_json:
             data = json.load(users_json)
 
             users_json.seek(0)
@@ -63,7 +45,7 @@ class Users:
 
             json.dump(data, users_json, indent=2)
 
-    def set_account(self, request_form: dict, login_mode: bool):
+    def set_account(self, request_form: dict, mode: str):
         """
         Interface that opens necessary files for needed function and does
         selected validation. It's created because we don't want to open
@@ -72,144 +54,105 @@ class Users:
         Parameters
         ----------
         request_form : dict
-            dict from html form
-        login_mode : bool
-            differs registration and login
+        mode : str
 
         Returns
         -------
         str | None
 
         """
-        with open(config.users_path, "r") as users_data:
-            self.user_data = json.load(users_data)
+        with open(self.users_path, "r") as users_json:
+            user_data = json.load(users_json)
 
-        self.request_form = request_form
+        return getattr(self, f"validate_{mode}")(request_form, user_data)
 
-        if login_mode:
-            return self.validate_login()
-        else:
-            return self.validate_registration()
-
-    def validate_phone_number(self) -> None | str:
+    def validate_phone_number(self, phone_number: str) -> None | str:
         """
         Validates phone number by using regex
 
         Parameters
         -----------
-        user_data: dict
+        phone_number : str
 
         Returns
         -------
         str | None
 
         """
-        if re.match(r"^[\d+\- ]{6,20}$", self.phone_number) is None:
+        if not re.match(r"^[\d+\- ]{6,20}$", phone_number):
             return """\t\t\tInvalid phone number.
                     Please use right format for phone number."""
 
-    def validate_password(self) -> None | str:
+    def validate_password(self, password: str) -> None | str:
         """
-        Validates password by using hash function
+        Validates password. Function makes sure that password is not empty
 
         Parameters
         -----------
-        None
+        password : str
 
         Returns
         -------
         str | None
 
         """
-        if not self.password:
+        if not password:
             return "Password is required."
 
-        if self.login_mode:
-            if not check_password_hash(
-                self.user_data[self.email]["password"], self.password
-            ):
-                return "Incorrect password"
+    def validate_email(self, email: str) -> None | str:
 
-    def validate_email(self) -> None | str:
-        """
-        Validates email by using regex
-
-        Parameters
-        -----------
-        None
-
-        Returns
-        -------
-        str | None
-
-        """
-        if not self.email:
-            return "Email is required"
-
-        if (
-            not self.login_mode
-            and re.match(
-                r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", self.email
-            )
-            is None
+        if not re.match(
+                r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",
+                email
         ):
             return "Invalid email format. Please use correct email format."
 
-        if self.login_mode:
-            if self.email not in self.user_data:
-                return "User with that email not found."
+    def validate_login(self, request_form: dict, user_data: dict) -> tuple:
+        """Validates user login by using existing password and email
 
-    def validate_login(self) -> tuple:
-        """Does user login validation by using another validation functions,
-        then if we have error return it inside of tuple with user_data dict
+        Parameters
+        ----------
+        request_form : dict
+        user_data : dict
 
         Returns
         -------
-        tuple[str | None, dict]
-            tuple that contains error and user data, we need user data
-            for session handling
+        tuple
+            Contains error end user data for further use in handler
         """
-        self.email: str = self.request_form["email"]
-        self.password: str = self.request_form["password"]
-        self.login_mode: bool = True
+        error = None
+        email = request_form["email"]
 
-        funclist = ("email", "password")
+        if email not in user_data:
+            return "User with that email not found.", user_data
 
-        for func in funclist:
-            error = getattr(self, f"validate_{func}")()
+        if not check_password_hash(
+            user_data[email]["password"],
+            request_form["password"]
+        ):
+            error = "Incorrect password", user_data
 
-            if error:
-                return error, self.user_data
+        return error, user_data
 
-        return None, self.user_data
-
-    def validate_password_repeat(self):
-        if self.password != self.password_repeat:
-            return "Passwords in both fields should be same."
-
-    def validate_registration(self) -> None | str:
+    def validate_registration(self, request_form: dict) -> None | str:
         """
         Validates registration by using some functions above
         If we have some errors in validation we will receive error as string
 
         Parameters
         -----------
-        None
+        request_form : dict
 
         Returns
         -------
         str | None
 
         """
-        self.email: str = self.request_form["email"]
-        self.phone_number: str = self.request_form["phone_number"]
-        self.username: str = self.request_form["username"]
-        self.password_repeat: str = self.request_form["password_repeat"]
-        self.password: str = self.request_form["password"]
+        if request_form['password'] != request_form['password_repeat']:
+            return "Passwords in both fields should be same."
 
-        funclist = ("email", "phone_number", "password_repeat", "password")
-        for func in funclist:
-            error = getattr(self, f"validate_{func}")()
+        for func in ("email", "phone_number", "password"):
+            error = getattr(self, f"validate_{func}")(request_form[func])
 
             if error:
                 return error
