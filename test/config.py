533f5e87d4f6eadd36cd17bd2c91ba10adc42c91
Aram-TA <aramg267@gmail.com>
2024-01-26 15:02:26 +0400

    Added normal file path support with os.path

--- test/config.py
+++ test/config.py
@@ -1,5 +1,6 @@
+import os
 """Configuration file for all constants that we will use inside project
 Add you'r constants if needed."""
 
-users_path = "data/users.json"
-posts_path = "data/posts.json"
+users_path = os.path.join("data", "users.json")
+posts_path = os.path.join("data", "posts.json")
